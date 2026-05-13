4f0544ef829bccd19d4f8ae51a55f9ea97589c1a
simayotlaca <simay.otlaca@sabanciuniv.edu>
2026-05-13 15:55:54 +0300

    update

--- Assets/Scripts/Wheel/Config/ZoneRewardTable.cs
+++ Assets/Scripts/Wheel/Config/ZoneRewardTable.cs
@@ -1,5 +1,17 @@
 using UnityEngine;
 
+[System.Serializable]
+public struct ZoneRewardEntry
+{
+    public RewardDefinition reward;
+
+    [Min(1)]
+    public int amount;
+
+    [Min(0f)]
+    public float weight;
+}
+
 [CreateAssetMenu(fileName = "ZoneRewardTable", menuName = "Wheel/ZoneRewardTable")]
 public class ZoneRewardTable : ScriptableObject
 {
